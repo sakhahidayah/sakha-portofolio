3c12550d862c97bde050028134e8992417285870
sakhahidayah <sakhahidayahramadhan@gmail.com>
2025-06-24 22:02:48 +0700

    download library react router ulang

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
+import {StrictMode } from 'react'
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./components/pages/Homepage.jsx";
 import NotFound from "./components/pages/NotFound.jsx";
 import AboutPage from "./components/pages/AboutPage.jsx";
@@ -24,5 +25,7 @@ const router = createBrowserRouter([
   },
 ]);
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>
 );
