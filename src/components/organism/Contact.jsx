89d5cdf80de23e5ee7801c7ad11d306015656ee5
sakhahidayah <sakhahidayahramadhan@gmail.com>
2025-06-27 19:33:33 +0700

    style: mengurangi space space yang kurang mengenakan

--- src/components/organism/Contact.jsx
+++ src/components/organism/Contact.jsx
@@ -3,7 +3,7 @@ import ContactGroup from "../molecules/ContactGroup";
 function Contact() {
   return (
     <>
-    <div id="contact" className="flex pt-32  flex-col gap-10 lg:gap-0 lg:flex-row-reverse justify-center lg:justify-between lg:px-16 lg:items-baseline items-center mb-24">
+    <div id="contact" className="flex pt-30  flex-col gap-10 lg:gap-0 lg:flex-row-reverse justify-center lg:justify-between lg:px-16 lg:items-baseline items-center ">
     <ContactGroup/>
     </div>
     </>
