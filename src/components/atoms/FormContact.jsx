005150f75129014afd2c9a3310f0b4e147ca17e3
sakhahidayah <sakhahidayahramadhan@gmail.com>
2025-07-21 15:00:30 +0700

    menambahkan focus outline pada form contact

--- src/components/atoms/FormContact.jsx
+++ src/components/atoms/FormContact.jsx
@@ -10,20 +10,20 @@ function FormContact() {
            action=""
             type="text"
             placeholder="Name"
-            className=" border border-[#8eebda] px-5 py-2 rounded-2xl"
+            className=" border border-[#8eebda] focus:outline-2 focus:outline-teal-400 px-5 py-2 rounded-2xl"
           />
           <input
             type="email"
             name=""
             id=""
             placeholder="Email"
-            className="border border-[#8eebda] px-5 py-2 rounded-2xl"
+            className="border border-[#8eebda] focus:outline-2 focus:outline-teal-400  px-5 py-2 rounded-2xl"
           />
           <textarea
             name=""
             id=""
             placeholder="Message..."
-            className="border border-[#8eebda] px-4 pt-3 pb-5 rounded-2xl "
+            className="border border-[#8eebda] focus:outline-2 focus:outline-teal-400 px-4 pt-3 pb-5 rounded-2xl "
           ></textarea>
         </form>
       </div>
