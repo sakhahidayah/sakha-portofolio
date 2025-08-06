5cb83df1a85bee77c59eddb2dbd186e61a3dabb6
sakhahidayah <sakhahidayahramadhan@gmail.com>
2025-08-06 11:08:29 +0700

    updated : menambahkan project iseng (Todolist App V2 )

--- src/components/molecules/ProjectsGroup.jsx
+++ src/components/molecules/ProjectsGroup.jsx
@@ -5,11 +5,12 @@ import Comics from "../atoms/Comics";
 import PortoReact from "../atoms/PortoReact";
 import PortoVersionOne from "../atoms/PortoVersionOne";
 import Todolist from "../atoms/Todolist";
+import TodolistV2 from "../atoms/Todolist-v2";
 
 function ProjectsGroup() {
   return (
     <>
-      <div  className="transition duration-300 flex flex-col gap-10 lg:flex-row lg:flex-wrap justify-center">
+      <div className="transition duration-300 flex flex-col gap-10 lg:flex-row lg:flex-wrap justify-center">
         <ChillMovie />
         <Calculator />
         <Comics />
@@ -17,6 +18,7 @@ function ProjectsGroup() {
         <ChillHtml />
         <PortoVersionOne />
         <PortoReact />
+        <TodolistV2 />
       </div>
     </>
   );
